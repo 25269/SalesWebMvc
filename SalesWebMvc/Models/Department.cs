7121d2ab4a6d0a2ac7420530dc3be8ba004e6b79
Cassiano Baldasso <baldassocassiano@gmail.com>
2025-01-20 22:40:53 -0300

    Primeiro model view controller

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿using SalesWebMvc.Controllers;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
