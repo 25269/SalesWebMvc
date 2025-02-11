e3c138e3ebd242e5dbaf022335f28331bc775c20
Cassiano Baldasso <baldassocassiano@gmail.com>
2025-02-10 22:35:16 -0300

    Criação de novas entidades e segunda migração feita para o banco

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,40 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateOnly birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sales)
+        {
+            Sales.Add(sales);
+        }
+
+        public void RemoveSales(SalesRecord sales)
+        {
+            Sales.Remove(sales);
+        }
+
+        public double TotalSales(DateOnly DateInitial, DateOnly DateFinal)
+        {
+            return Sales.Where(Sales => Sales.Date >= DateInitial && Sales.Date <= DateFinal).Sum(Sales => Sales.Amount);
+        }
+    }
+}
