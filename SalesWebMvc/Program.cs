623d7048a51e1cdf26d7517231783dd5892f8e20
Cassiano Baldasso <baldassocassiano@gmail.com>
2025-02-17 23:11:57 -0300

    Criacao do SellerFormViewModel e selecao dos departamentos para preenchimento de combobox na interface

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -23,6 +23,7 @@ namespace SalesWebMvc
             //Adicionar ao escopo do projeto o serviço de inserção dos dados ao BD
             builder.Services.AddScoped<SeedingService>();
             builder.Services.AddScoped<SellerService>();
+            builder.Services.AddScoped<DepartmentService>();
 
             var app = builder.Build();
 
